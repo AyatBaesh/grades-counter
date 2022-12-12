3b3bd27c6e215c0d76a9c64a3accf31d636db79f
Ayat <ayatbaeshev@gmail.com>
2022-12-12 16:01:13 +0100

    changed iteration through array using the .forEach() function

--- scripts/stats.js
+++ scripts/stats.js
@@ -7,6 +7,9 @@
 //Math.max(... array) = searching max value in given array. Manual algorithm is: assign the first element of the array to the max variable,
 // then iterate through the array and compare it to the others. If bigger element found - reassign max variable.
 
+// array.forEach(function(array_element){
+//    action needed to be nade to every array element
+//})
 
 
 function getNumberOfGrades(grades) {
@@ -15,24 +18,24 @@ function getNumberOfGrades(grades) {
 
 function getAverageGrade(grades) {
     let result = 0;
-    for(let i in grades){
-        result += parseInt(grades[i],10);
-    }
+    grades.forEach((grade) =>{//iterating through grades with the help of forEach
+        result += grade;
+})
     let averageGrade = result/grades.length;
     return parseFloat(averageGrade.toFixed(1));
-}
 
+}
 function highestGrade(grades) {
     return parseInt(Math.max(...grades),10);
 }
 
 function tooEasy(grades){//test considered too easy if  50% or more students scored max grade
     let maxGradesAmount = 0;
-    for(let i in grades){//counting the amount of max grades scored
-        if(parseInt(grades[i],10) === 20){
+    grades.forEach((grade) => {//counting the amount of max grades scored
+        if(grade === 20){
             maxGradesAmount+=1;
         }
-    }
+    })
     
     if(grades.length%2 === 0){
         return maxGradesAmount >= grades.length/2;
@@ -44,11 +47,11 @@ function tooEasy(grades){//test considered too easy if  50% or more students sco
 function amountOfGrades(grades,chosenGrade){//counting the total amount of scored chosen grades
     let chosenGradeValue = parseInt(chosenGrade.value,10);
     let gradesAmount = 0;
-    for(let i in grades){
-        if(chosenGradeValue === parseInt(grades[i],10) ){
+    grades.forEach((grade)=>{
+        if(chosenGradeValue === grade){
             gradesAmount+=1;
         }
-    }
+    })
     return gradesAmount;
 }
 
